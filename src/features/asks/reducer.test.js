d78d26b66439fbbf45a5864a9d2e8ad0b5da9082
Sam Frances <sam@samfrances.uk>
2018-08-28 22:16:54 +0100

    Directory rename and linting

--- src/features/asks/reducer.test.js
+++ src/features/asks/reducer.test.js
@@ -1,42 +1,42 @@
-import { describe } from 'riteway'
-import { map, pickAll } from 'ramda'
+import { describe } from 'riteway';
+import { map, pickAll } from 'ramda';
 
-import { acceptAsk, addAsk, rejectAsk, updateAsk } from './actions'
-import { defaultState, questionsReducer as reducer } from './reducer'
+import { acceptAsk, addAsk, rejectAsk, updateAsk } from './actions';
+import { defaultState, questionsReducer as reducer } from './reducer';
 
-const expectedProps = map(pickAll(['askee', 'question', 'status']))
+const expectedProps = map(pickAll(['askee', 'question', 'status']));
 
 describe('questionsReducer()', async should => {
-  const { assert } = should()
+  const { assert } = should();
 
   assert({
     given: 'no state or action',
     should: 'return the default state',
     actual: reducer(),
     expected: defaultState,
-  })
+  });
 
   assert({
     given: 'current state and an unknown action type for this reducer',
     should: 'return the current state',
     actual: reducer({ some: 'state' }, { type: '__unknown__' }),
     expected: { some: 'state' },
-  })
+  });
 
   {
     const expected = [
       { askee: 'Boss', question: 'Can I have a raise?', status: 'Unanswered' },
-    ]
-    const action = addAsk({ question: 'Can I have a raise?', askee: 'Boss' })
+    ];
+    const action = addAsk({ question: 'Can I have a raise?', askee: 'Boss' });
 
-    const nextState = reducer(undefined, action)
-    const actual = expectedProps(nextState)
+    const nextState = reducer(undefined, action);
+    const actual = expectedProps(nextState);
 
     assert({
       given: `a state with zero asks and a ${addAsk().type} action`,
       should: 'return a new state containing the ask',
       actual,
       expected,
-    })
+    });
   }
-})
+});
